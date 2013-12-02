a0cdace794d70158a5c8d240ca242579dd843958
GarfieldLinux <garfieldlinux@gmail.com>
2013-12-02 22:33:14 +0800

    remove  unusing shell file by garfield

--- kernel_api_server/server.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-g++ -I. -I$ICE_HOME/include  -lpthread  -c ../kernel_ice/Ipmstor.cpp server.cpp netmask.cpp errors.cpp 
-g++  -o server Ipmstor.o server.o errors.o netmask.o  -L$ICE_HOME/libg -lIce -lIceUtil  -lmysqlclient -I/usr/include/mysql/ -L/usr/lib64/mysql ./IPMS_config_so/config.so 
